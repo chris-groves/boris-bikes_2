bd2cd5043769cd97356194ccc9b48e59da55d542
Chris Groves <chris_groves87@hotmail.co.uk>
2020-09-02 16:42:11 +0100

    Refactor

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,12 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    raise 'Dockingstation is empty' if @docked_bikes.empty?
+    raise 'Dockingstation is empty' if empty?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    raise 'Dockingstation is full' if @docked_bikes.length > 0
+    raise 'Dockingstation is full' if full?
     @docked_bikes << bike
+  end
+
+  private
+
+  def full?
+    @docked_bikes.length > 0
+  end
+
+  def empty?
+    @docked_bikes.length < 1
   end
 end
