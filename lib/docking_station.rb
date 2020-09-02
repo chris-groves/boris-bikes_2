3106aa9f81e147cc5bb17505e7957f9fa556d756
Chris Groves <chris_groves87@hotmail.co.uk>
2020-09-02 16:30:55 +0100

    Prevent empty dockingstations from releasing bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
 class DockingStation
-  attr_reader :docked_bikes
+  attr_accessor :docked_bikes
 
   def initialize
     @docked_bikes = []
   end
 
   def release_bike
-    Bike.new
+    raise 'Dockingstation is empty' if @docked_bikes.empty?
+    @docked_bikes.pop
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,26 @@
 require 'docking_station'
+require 'pry'
 
 describe DockingStation do
-  it { expect(subject).to respond_to :release_bike }
-
   it 'releases a working bike' do
-    bike = subject.release_bike
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    station.release_bike
     expect(bike).to be_working
   end
 
   it 'it docks bikes' do
     docking_station = DockingStation.new
-    bike = subject.release_bike
+    bike = Bike.new
     docking_station.dock(bike)
     expect(docking_station.docked_bikes).to eq [bike]
+  end
+
+  describe '#release_bike' do
+    it 'prevents the release of bikes if docking station is empty' do
+      station = DockingStation.new
+      expect { station.release_bike }.to raise_error('Dockingstation is empty')
+    end
   end
 end
